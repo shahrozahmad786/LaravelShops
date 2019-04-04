0c42bb7bb902115eee7cd60faf5d2b7fa4820ced
shahroz <shahrozahmad786@gmail.com>
2019-04-04 22:24:05 +0500

    Initial commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role1= Role::create([
+            'name'=>'admin',
+            'display_name'=>'admin',
+
+
+        ]);
+
+
+        $role2= Role::create([
+            'name'=>'user',
+            'display_name'=>'user',
+
+
+        ]);
+
+        $user1 =User::create([
+              
+            'email'=>'admin@admin.com', 
+            'password'=>bcrypt('123456'),
+            'role_id'=>$role1->id,
+
+        ]);
+        $user1->roles()->attach($role1->id);
+         
+        $user2 =User::create([
+              
+            'email'=>'testuser@gmail.com', 
+            'password'=>bcrypt('123456'),
+            'role_id'=>$role2->id,
+
+        ]);
+        $user2->roles()->attach($role2->id);
+
+
+    }
+}
